4b145a9dfc51045a9f3f66ba3a00d4a99410b8b6
xieshuhong <364118360@qq.com>
2022-12-15 13:23:34 -0700

    updates codes

--- read_library_csv.py
+++ read_library_csv.py
@@ -1,8 +1,15 @@
 import csv
+import copy
 from library import Library
+# from rich.progress import track
+# import time
 
+# for counter in track(range(5), description="Completion..."):
+#    print(f"Loading data")
+#    time.sleep(0.5)
 
 libraries = []
+title_info = '\n{:<30} {:<20} {:<20} {:<20}\n'.format('Library', 'Postal Code', 'Square Feet', 'Phone Number')
 
 # load data from the csv file to the libraries list
 def load_csv():
@@ -11,28 +18,32 @@ def load_csv():
         line_count = 0
         for row in csv_reader:
             if line_count != 0:
-                library = Library(row[0], row[1], row[2])
+                library = Library(row[0], row[1], row[2], row[3])
                 libraries.append(library)
-
             line_count += 1
 
 
 
-# sort libraries by their names in descending order
-def sort_library_by_name_asc():
-     libraries.sort(key=lambda x: x.name, reverse = True)
+# display and sort libraries by their names in ascending order
+def display_and_sort():
+    #  deep copy of the list
+     copy_libraries = [x for x in libraries]
+     copy_libraries.sort(key=lambda x: x.name)
+     print(title_info)
+     for library in copy_libraries:
+        print(library)
 
 
 # find the library you want to find
 def find_a_library():
+    library_bool = False
     library_name = input("Please enter the name of the library that you want to find information on:\n")
     for library in libraries:
         if library.name == library_name:
-            print('We have found that library, here is all the relevant information:\n', library)
-
-def format_libraries():
-    for library in libraries:
-        print(library)
+            library_bool = True
+            print(f'\nWe have found that library, here is all the relevant information:\n{title_info}{library}\n')
+    if not library_bool:
+            print('\nSorry, please enter valid library name!')
 
 
 
@@ -49,29 +60,34 @@ def find_nearest(post_code, library):
 nearby_libraries = {}
 #  find the nearest library the users wants
 def find_nearest_library():
-    post_code = input('please input your postal code(i.e. T2W 4N1) so we find the nearest library for you:\n')
-    for i, library in enumerate(libraries):
-        matched_letters = find_nearest(post_code, library)
-        library.matched_letters = matched_letters
-        nearby_libraries[i] = library
+    # reverse it back first
+        post_code = input('\nplease input your postal code(i.e. T2W 4N1) so we find the nearest library for you:\n')
+        for i, library in enumerate(libraries):
+            try:
+                matched_letters = find_nearest(post_code, library)
+                library.matched_letters = matched_letters
+                nearby_libraries[i] = library
+            except:
+                print('\nPlease input the right postal code!')
 
 
 
-
+# get the data from the library csv
 load_csv()
-sort_library_by_name_asc()
-# print()
-# format_libraries()
+# display and sort libraries by their names in ascending order
+display_and_sort()
+
 # find_a_libraries()
 
 def sorted_function(x):
      return x[1].matched_letters
 
 
-
+# find the nearest library according to the postal code
 find_nearest_library()
 sorted_matched_libraries = sorted(nearby_libraries.items(), key =sorted_function, reverse = True)
-# print('sorted_matched_libraries', sorted_matched_libraries)
-print("The closest library is", sorted_matched_libraries[0][1].name)
+print("The closest library is", sorted_matched_libraries[0][1].name + '\n')
 
 
+# find the library info according to the library's name
+find_a_library()
